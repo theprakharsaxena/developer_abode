cb555b92b5b373a0942d2d8e2cb75a3d21ace2c5
Prakhar Saxena <prakharsaxena5125@gmail.com>
2024-08-24 00:22:47 +0530

    update

--- api/config/config.ts
+++ api/config/config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  port: process.env.PORT || 5000,
+  port: process.env.PORT || "5000",
   mongodbUri: process.env.MONGODB_URI || "",
   jwtSecret: process.env.JWT_SECRET || "",
   jwtExpiration: process.env.JWT_EXPIRATION || "1h",
