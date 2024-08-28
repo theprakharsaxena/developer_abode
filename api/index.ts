be033d6ea476b7959b85ad7ac4688993a41c97c7
Prakhar Saxena <prakharsaxena5125@gmail.com>
2024-08-28 12:33:05 +0530

    update

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import { config } from "./config";
 import app from "./app";
+import mongoose from "mongoose";
 
 dotenv.config();
 const PORT = config.port || 3000;
@@ -9,8 +10,19 @@ app.get("/", (req, res) => {
   res.json({ message: "Development: Server is running" });
 });
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(config.mongodbUri);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+};
+
 // Start the server for local development
 const startServer = async () => {
+  await connectDB();
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
