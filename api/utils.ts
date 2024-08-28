a7f23dfb44bf5ab12d6b4e80fa86263ccce2e403
Prakhar Saxena <prakharsaxena5125@gmail.com>
2024-08-28 12:41:47 +0530

    update utils and middleware

--- api/utils.ts
+++ api/utils.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import { config } from "./config";
+import jwt from "jsonwebtoken";
+import nodemailer from "nodemailer";
 
 export const connectDB = async () => {
   try {
@@ -9,4 +11,35 @@ export const connectDB = async () => {
     console.error("Error connecting to MongoDB:", error);
     process.exit(1);
   }
+};
+
+export const generateToken = (userId: string) => {
+  return jwt.sign({ id: userId }, config.jwtSecret, {
+    expiresIn: config.jwtExpiration,
+  });
+};
+
+export const verifyToken = (token: string) => {
+  return jwt.verify(token, config.jwtSecret);
+};
+
+export const sendEmail = async (to: string, subject: string, text: string) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail", // You can use other services or SMTP settings
+    secure: true,
+    port: 465,
+    auth: {
+      user: config.emailUser,
+      pass: config.emailPassword,
+    },
+  });
+
+  const mailOptions = {
+    from: config.emailUser,
+    to,
+    subject,
+    text,
+  };
+
+  await transporter.sendMail(mailOptions);
 };
