ec68fedb83fa929112adc927fdf4c373a145b01e
Prakhar Saxena <prakharsaxena5125@gmail.com>
2024-08-28 12:52:14 +0530

    update routes

--- api/app.ts
+++ api/app.ts
@@ -1,5 +1,12 @@
 import express from "express";
 import helmet from "helmet";
+import {
+  authRouter,
+  enrollmentRouter,
+  internshipRouter,
+  taskRouter,
+  userRouter,
+} from "./router";
 
 const app = express();
 
@@ -7,10 +14,10 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 
-// app.use("/api/v1/auth", authRouter);
-// app.use("/api/v1/users", userRouter);
-// app.use("/api/v1/internships", internshipRouter);
-// app.use("/api/v1/tasks", taskRouter);
-// app.use("/api/v1/enrollments", enrollmentRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/internships", internshipRouter);
+app.use("/api/v1/tasks", taskRouter);
+app.use("/api/v1/enrollments", enrollmentRouter);
 
 export default app;
